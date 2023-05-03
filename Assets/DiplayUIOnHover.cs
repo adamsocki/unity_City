b422e7bde437f9b8dfb7b926513a90d723f94ada
adamsocki <socki.adam@gmail.com>
2023-05-02 23:14:14 -0500

    ui completed

---
+++ Assets/DiplayUIOnHover.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DiplayUIOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public GameObject panel; // Assign the panel in the Inspector
+
+    // This method is called when the mouse pointer enters the button
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        panel.SetActive(true);
+        Debug.Log("Over");
+    }
+
+    // This method is called when the mouse pointer exits the button
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        panel.SetActive(false);
+        Debug.Log("Off");
+
+    }
+}
